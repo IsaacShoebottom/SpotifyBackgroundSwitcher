96e21dfcb445b17b511be98f0670e001c6366ac3
Isaac Shoebottom <ir.shoebottom@gmail.com>
2022-07-03 01:38:46 -0300

    60 fps animation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // @name         Spotify Background Changer
 // @namespace    http://tampermonkey.net/
 // @source       https://github.com/Glaceon575/SpotifyBackgroundSwitcher
-// @version      0.1.2
+// @version      0.1.3
 // @description  Changes the background of Spotify playlists and albums in a rainbow pattern
 // @author       Isaac Shoebottom
 // @updateURL    https://raw.githubusercontent.com/Glaceon575/SpotifyBackgroundSwitcher/master/script.js
@@ -17,7 +17,7 @@ let background;
 let colorInterval;
 
 const selector = ".gHImFiUWOg93pvTefeAD.xYgjMpAjE5XT05aRIezb";
-const timer = 33; //in milliseconds
+const timer = 16; //in milliseconds
 const probeTimer = 500 //in milliseconds, timer for checking for selector
 const difference = 60; //delta between the top and bottom
 
